f073bedda72a2c48cc1126de5ee4195deafa6387
haidangdev2308 <haidang23082002@gmail.com>
2023-06-27 10:54:12 +0700

    v5

--- src/components/SidebarLeft.js
+++ src/components/SidebarLeft.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import { menuSidebar } from '../ultis/menu'
+import { NavLink } from 'react-router-dom' //NavLink để viết những link trong navbar
+
+const notActiveStyle = 'hover:text-[#b72479] border-solid border-transparent border-l-[3px] py-[12px] px-[21px] font-medium flex items-center gap-3 text-[15px] text-[#4d4b54]'
+const activeStyle = 'border-solid border-l-[3px] py-[12px] px-[21px] font-medium flex items-center gap-3 text-[15px] text-[#b72479] border-[#b72479] bg-[rgba(0,0,0,0.05)]'
 
 const SidebarLeft = () => {
   return (
-    <div className='text-white'>
+    <div className='flex flex-col bg-[#fbe6e6]'>
         <div className='w-full pl-[25px] pr-[28px] h-[70px] flex items-center'>
             <img src={logo} alt="logo" className='w-[120px] h-[40px]' />
+        </div>
+
+        <div className=' flex flex-col'>
+          {
+            menuSidebar.map( item => (
+              <NavLink
+                to={item.path}
+                key={item.path}
+                end={item.end}
+                  className= { ({isActive}) => isActive? activeStyle : notActiveStyle } //  biến isActive là của NavLink
+              >
+                {item.icon}
+                <span>{item.text}</span>
+              </NavLink>
+            ))
+          }
         </div>
     </div>
   )
--- src/ultis/menu.js
+++ src/ultis/menu.js
@@ -1,6 +1,22 @@
-const menuSidebar = [
+import icons from "./icons"
+
+const { MdOutlineLibraryMusic } = icons
+
+export const menuSidebar = [
     {
-        path: 'personal',
-        text: 'cá nhân',
+        path: '',
+        text: 'Khám Phá',
+        end: true,
+        icon: <MdOutlineLibraryMusic size={24} /> ,
+    },
+    {
+        path: 'zing-chart',
+        text: '#zingchart',
+        icon: <MdOutlineLibraryMusic size={24} />,
+    },
+    {
+        path: 'mymusic',
+        text: 'Thư Viện',
+        icon: <MdOutlineLibraryMusic size={24} />,
     },
 ]
